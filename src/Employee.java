d6245bd8a3c84f28417c2920ceb76ea37c2a5df2
annibelll <annaemelanova137@gmail.com>
2024-10-30 11:47:13 +0100

    Task

--- src/Employee.java
+++ src/Employee.java
@@ -2,15 +2,18 @@ public class Employee implements Person {
     private String name;
     private String surname;
     private int age;
+    private int retirement_age;
 
     public Employee(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
+        this.retirement_age = 62 - age;
     }
 
     @Override
     public void getInfo() {
-        System.out.println("Name: " + name + " Surname: " + surname + " Age: " + age);
+        System.out.println("Name: " + name + " Surname: " + surname + " Age: " + age + " Years until retirement: " + retirement_age);
     }
 }
+
