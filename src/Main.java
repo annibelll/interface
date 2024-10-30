1e7cf7c6f452c38a2db678419be65ce8ba258480
annibelll <annaemelanova137@gmail.com>
2024-10-30 11:38:03 +0100

    Task

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Создаём объект класса Student
+
         Person student = new Student("Anna", "Bieliaieva", 19, 12345);
         student.getInfo();
 
-        // Создаём объект класса Employee
+
         Person employee = new Employee("Kate", "Bla", 25);
         employee.getInfo();
     }
